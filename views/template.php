4434c6aeb6f98d3cc4f9759c48cf4774877727bf
Mansy Théo <72173647+Kamesky@users.noreply.github.com>
2022-03-20 17:00:21 +0100

    Update title

--- views/template.php
+++ views/template.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" type="text/css" href="/public/css/bootstrap.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
     <link rel="stylesheet" type="text/css" href="/public/css/style.css">
-    <title>Hello, world!</title>
+    <title>Café bio en ligne - Costa Coffee</title>
   </head>
 
   <body>
